2358ed64c814dd9da313798ff1aac0fe9d63ad90
githubgajen <github.gajen@gmail.com>
2024-04-24 10:13:52 -0400

    changed the title from React App to Task Tracker

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      
         <a
           className="App-link"
           href="https://reactjs.org"

142bf582ff855d6d9d41a9d991167e930d281823
githubgajen <github.gajen@gmail.com>
2024-04-24 11:02:07 -0400

    ternary operator demo in app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
+import React, { useState} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ 
+  const [isClicked, setIsClicked] = useState(false);
+
+
   return (
-    <div className="">
+
+    <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <label onClick={()=> setIsClicked(!isClicked)}
+      style={{ fontSize: isClicked ? '100px' : '16px' }}
+      >
+        {isClicked ? 'Lado chu radi !!! Ghata muji !!!' : 'Click Here'}
+      </label>
     </div>
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+
+    
   );
 }
 
